a7328024b1f2a003de492dd6d6e089d08461f529
Wayner Valverde <waystanley14@hotmail.es>
2016-05-25 20:54:26 -0600

    Se agrega el Factory

---
+++ Proyecto2/src/Modelo/FactoryServicio.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import DTOs.DTOServicioAmortizacion;
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ *
+ * @author way14
+ */
+public class FactoryServicio {
+    
+    public FactoryServicio(){}
+    
+    public ServicioAmortizacion createServicioAmortizacion(DTOServicioAmortizacion servicio) throws ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        
+         return (ServicioAmortizacion) Class.forName("modelo."+servicio.getTipo()).getConstructor(DTOServicioAmortizacion.class).newInstance(servicio);
+    }
+    
+}
